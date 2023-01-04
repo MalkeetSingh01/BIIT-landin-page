e86b8434aa853585a1a90c0a3e48eb0b2e516694
MalkeetSingh01 <80948821+MalkeetSingh01@users.noreply.github.com>
2023-01-04 10:29:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// const express =require("express");
+// const path=require("path");
+// const app=express();
+// // const mongoose = require('mongoose');
+// const bodyparser=require("body-parser");
+
+// const mongoose = require('mongoose');
+
+// main().catch(err => console.log(err));
+
+// async function main() {
+//   await mongoose.connect('mongodb://localhost/loginblock');
+// }
+// const port=8000;
+
+// // EXPRESS RELATED STUFF
+// app.use('/static',express.static('static'))
+// // for serving static files
+// app.use(express.urlencoded())
